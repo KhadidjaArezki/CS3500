9f8c207c8b0ba1ef7f6d803a18aa7abf38da0664
KhadidjaArezki <khadidjaarezki999@gmail.com>
2024-08-04 23:39:19 +0100

    Add solution to assignment 3

---
+++ Assignment03/src/cs3500/pyramidsolitaire/model/hw02/Card.java
@@ -0,0 +1,36 @@
+package cs3500.pyramidsolitaire.model.hw02;
+
+import java.util.Objects;
+
+public final class Card {
+  String name;
+  int value;
+  String suit;
+  
+  public Card (String name, int value, String suit) {
+    this.name = name;
+    this.value = value;
+    this.suit = suit;
+  }
+   @Override
+  public String toString() {
+     return this.name + this.suit;
+  }
+  
+  @Override
+  public boolean equals(Object that) {
+    if (this == that) {
+      return true;
+    }
+
+    if (!(that instanceof Card)) {
+      return false;
+    }
+    return this.name == ((Card) that).name && this.value == ((Card) that).value && this.suit == ((Card) that).suit;
+  }
+  
+  @Override
+  public int hashCode() {
+    return Objects.hash(this.name, this.suit);
+  }
+} 
